fa35092827198cc192365d6f4efccddadf39365c
cccaaannn <can.kurt.aa@gmail.com>
2025-12-16 08:10:26 +0300

    login requests

---
+++ Data/LoginRequest.cs
@@ -0,0 +1,7 @@
+namespace CompanyRatingFrontend.Data;
+
+public record LoginRequest
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
---
+++ Data/RegisterRequest.cs
@@ -0,0 +1,9 @@
+namespace CompanyRatingFrontend.Data;
+
+public record RegisterRequest
+{
+    public string Email { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Surname { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
